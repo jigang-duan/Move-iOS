0ced04b8928006aaba5a9f1ab01d3ba432da4128
haoyong.xin <haoyong.xin@jrdcom.com>
2017-03-24 17:13:49 +0800

    revise url schemes

--- Move App/Move App/App Delegate/AppDelegate.swift
+++ Move App/Move App/App Delegate/AppDelegate.swift
@@ -49,5 +49,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
+    func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
+        print("\(url.absoluteString)")
+        return true
+    }
 }
 
