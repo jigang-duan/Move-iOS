2e8e2419e415881d2a4f413f1735b6ddf41a9d13
jigang.duan <jigang.duan@tcl.com>
2017-05-27 15:14:25 +0800

    add mail

--- Move App/Move App/Source Code/Scenes/Account/Controller/AboutViewController.swift
+++ Move App/Move App/Source Code/Scenes/Account/Controller/AboutViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class AboutViewController: UIViewController {
     
@@ -20,4 +21,31 @@ class AboutViewController: UIViewController {
         }
     }
     
+    @IBAction func logDidTap(_ sender: UITapGestureRecognizer) {
+        guard MFMailComposeViewController.canSendMail() else {
+            print("Mail services are not available")
+            return
+        }
+        let composeVC = MFMailComposeViewController()
+        composeVC.mailComposeDelegate = self
+        
+        // Configure the fields of the interface.
+        composeVC.setToRecipients(["jigang.duan@tcl.com"])
+        composeVC.setSubject("Hello!")
+        composeVC.setMessageBody("Hello from California!", isHTML: false)
+        
+        // Present the view controller modally.
+        self.present(composeVC, animated: true, completion: nil)
+    }
+}
+
+// MARK: delegate
+extension AboutViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        // Check the result or perform other tasks.
+        
+        // Dismiss the mail compose view controller.
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
