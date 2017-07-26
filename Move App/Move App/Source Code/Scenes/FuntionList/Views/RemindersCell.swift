9e278b032af1faffa4592ef26d4d922fe6ee7b02
junmiao.huang <junmiao.huang@jrdcom.com>
2017-07-26 11:26:23 +0800

    fix alarm bug and TimeZone bug

--- Move App/Move App/Source Code/Scenes/FuntionList/Views/RemindersCell.swift
+++ Move App/Move App/Source Code/Scenes/FuntionList/Views/RemindersCell.swift
@@ -72,16 +72,16 @@ class RemindersCell: UITableViewCell {
                 s += " "
             }
         }
-        if s == R.string.localizable.id_week_monday()+R.string.localizable.id_week_tuesday()+R.string.localizable.id_week_wednesday()+R.string.localizable.id_week_thurday()+R.string.localizable.id_week_friday()
+        if s == R.string.localizable.id_week_monday()+" "+R.string.localizable.id_week_tuesday()+" "+R.string.localizable.id_week_wednesday()+" "+R.string.localizable.id_week_thurday()+" "+R.string.localizable.id_week_friday()+" "
         {
             s = R.string.localizable.id_week_school_day()
         }
-       else if s == R.string.localizable.id_week_sunday()+R.string.localizable.id_week_monday()+R.string.localizable.id_week_tuesday()+R.string.localizable.id_week_wednesday()+R.string.localizable.id_week_thurday()+R.string.localizable.id_week_friday()+R.string.localizable.id_week_saturday()
+       else if s == R.string.localizable.id_week_sunday()+" "+R.string.localizable.id_week_monday()+" "+R.string.localizable.id_week_tuesday()+" "+R.string.localizable.id_week_wednesday()+" "+R.string.localizable.id_week_thurday()+R.string.localizable.id_week_friday()+" "+R.string.localizable.id_week_saturday()+" "
             
         {
             s = R.string.localizable.id_week_everyday()
         }
-       else if s == R.string.localizable.id_week_sunday()+R.string.localizable.id_week_saturday()
+       else if s == R.string.localizable.id_week_sunday()+" "+R.string.localizable.id_week_saturday()+" "
         {
             s = R.string.localizable.id_week_weekend()
         }
